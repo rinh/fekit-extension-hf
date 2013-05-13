11cc375d62a88e64c733186f2400aa1ff43d4543
rinh <hao.lin@qunar.com>
2013-05-13 18:46:05 +0800

    initialize

--- README.md
+++ README.md
@@ -1,71 +1,28 @@
-FEKIT
+fekit-extension-hf
 =====================
 
-### fekit extension是什么? ###
+### 说明 ###
 
-有时你希望增加一些命令给 fekit，但是大多数情况下并不想把它加入 fekit project 官方命令中。 
+处理header&footer项目编译使用
 
-所以，fekit extension为你提供了这样一种办法来扩展命令。
+### 使用方法  ###
 
-## 如何开发？ ###
+在 hf 项目中，调用 
 
-请先将本项目 clone 到本地(假设要开发一个svn扩展)
+    fekit hf 
 
-    git clone git@github.com:rinh/fekit-extension-template.git ./fekit-extension-svn
+即可压缩并混淆
 
-本项目是 extension标准模板，以下文件是你需要修改的。
+### hf 格式简介 ###
 
-* index.js  --- 扩展入口文件，主要功能需要写在这里
-* package.json  --- npm 配置文件
-* README.md  --- 说明文档，简单介绍一下你的功能
+在 html 代码中，可以使用以下几种 hook
 
-#### package.json  ####
+{{ver}} - 替换为当前编译时间戳
 
-    {
-        // *必填项* 扩展命令名称
-        "fekit_extension_command_name":"",
-        // *必填项* 例 fekit-extension-svn
-        "name": "fekit-extension-template",
-        // *必填项* 请维护你的依赖
-        "version": "0.0.0"
-    }
+{{js path}} - 替换为`path`的文件，压缩后的javascript代码
 
-## 如何发布? ###
+{{css path}} - 替换为`path`的文件，压缩后的css代码
 
-    npm publish 
+{{html path}} - 替换为`path`的文件，处理后的html代码
 
-## 如何使用? ###
-
-以 svn 为例
-
-package.json
-
-    {
-        // *必填项* 扩展命令名称
-        "fekit_extension_command_name":"svn",
-        // *必填项* 例 fekit-extension-svn
-        "name": "fekit-extension-svn",
-        // *必填项* 请维护你的依赖
-        "version": "0.1.0"
-    }
-
-命令行
-
-    npm install fekit-extension-svn -g
-
-    fekit svn --help
-
-
-### 安装时的一些问题 ###
-
-如果出现
-
-    sh: node: Permission denied
-
-这样的提示，请使用
-
-    npm config set user 0
-    npm config set unsafe-perm true
-
-
-
+以上`path`均使用相对路径
--- index.js
+++ index.js
@@ -1,9 +1,118 @@
-exports.usage = ""
+require('date-utils');
+var fs = require('fs')
+var syspath = require('path')
+var baselib = syspath.join( module.parent.filename , '../' )
+var utils = require( syspath.join( baselib , 'util'  ) )
+var minCode = require( syspath.join( baselib , 'commands/min' ) ).minCode
+
+var VER = new Date().toFormat('YYYYMMDDHH24MISS')
+
+exports.usage = "处理header&footer项目编译使用";
 
 exports.set_options = function( optimist ){
+
     return optimist
 }
 
 exports.run = function( options ){
+
+    var reg = /src\/.*?\/.*.html$/;
+
+    utils.path.each_directory( process.cwd() , function( path ){ 
+        if( reg.test( path ) ) {
+            build_file( path );
+        }
+    } , true );
+
+    utils.logger.log('done.')
+
+}
+
+function build_file( path ) {
+    var src = compileHTML( path ) 
+    var prdfile = path.replace('/src/','/prd/');
+    utils.file.mkdirp( utils.path.dirname( prdfile ) )
+    utils.file.io.write( prdfile , src )
+    utils.logger.log( path + " done." )
+}
+
+function compileHTML( path ) {
+
+    var result = [];
+
+    var basePath = utils.path.dirname( path );
+
+    var htmlSrc = utils.file.io.read( path )
+
+    htmlSrc = grep( htmlSrc , 'space' )
+
+    htmlSrc = grep( htmlSrc , 'ver' )
+
+    htmlSrc = grep( htmlSrc , 'html' , function( path ){
+        var f = combine_path( basePath , path );
+        return compileHTML( f )
+    })
+
+    htmlSrc = grep( htmlSrc , 'js' , function( path ){
+        var f = combine_path( basePath , path );
+        return minCode( ".js" , utils.file.io.read( f ) )
+    })    
+
+    htmlSrc = grep( htmlSrc , 'css' , function( path ){
+        var f = combine_path( basePath , path );
+        return minCode( ".css" , utils.file.io.read( f ) , { noSplitCSS : true } )
+    })    
+
+    return htmlSrc;
+
+}
+
+
+function grep( src , type , cb ) {
+
+    switch( type ) {
+
+        case "space":
+            return src.replace( /\s{2,}/g , " " )
+        case "ver":
+            return src.replace( /\{\{ver\}\}/g , VER )
+        case "js":
+            return src.replace( /\{\{js (.*?)\}\}/g , function($0, $1){
+                return cb($1);
+            });
+        case "css":
+            var _src = src.replace( /\{\{css (.*?)\}\}/g , function($0, $1){
+                return cb($1);
+            });
+            return fix_Matrix( _src );
+        case "html":
+            return src.replace( /\{\{html (.*?)\}\}/g , function($0, $1){
+                return cb($1);
+            });
+
+    }
+
+}
+
+
+function combine_path( basePath , filePath ) {
+    if( utils.path.exists( filePath ) ) {
+        return filePath;
+    } else {
+        var full = utils.path.join( basePath , filePath );
+        if( utils.path.exists( full ) ) {
+            return full
+        }
+    }
+    throw "[ERROR]路径不存在。 " + basePath + " " + filePath;
+}
+
+
+function fix_Matrix( src ) {
+
+    var reg = /DXImageTransform.Microsoft.Matrix.*?\)/ig
+    return src.replace( reg , function($0){
+        return $0.replace(/,/g,', ');
+    })
 
 }
